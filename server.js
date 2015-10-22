5b0f2fcc08bc126ebd45bdb4d3561debcb1df203
Naim Falandino <lnfalandino@gmail.com>
2015-10-22 11:46:36 -0400

    Remove server script in favor of http-server module for testing

--- server.js
+++
@@ -1,8 +0,0 @@
-var connect = require('connect');
-
-var app = connect()
-    .use(connect.logger('dev'))
-    .use(connect.static(__dirname))
-    .use(connect.directory(__dirname));
-
-connect.createServer(app).listen(8080);
